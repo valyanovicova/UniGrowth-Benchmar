67df3da3ac01c4d5faebf6e5e706ff24b86414bf
Ramis <ramis@Ramiss-MacBook-Air.local>
2025-05-10 16:25:05 +0300

    change geocoding api to places api

--- location/parser/location_parser.py
+++ location/parser/location_parser.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 import time
+import aiohttp
+import asyncio
 import os
 from settings import settings
 from logger import logger
@@ -21,49 +23,51 @@ class UniversityExcelFile:
         return self.data["University Name"].to_list()
 
     def get_name_and_location(self):
-        return self.data.set_index("University Name")["Location"].to_dict()
+        return self.data.set_index("University Name")["Location"].fillna(None).to_dict()
 
     def get_name_and_address(self):
-        return self.data.set_index("University Name")["Address"].to_dict()
+        return self.data.set_index("University Name")["Address"].fillna(None).to_dict()
 
 
 class GEOCoordinateParserByName:
-    def __init__(self, names):
+    def __init__(self, names: list):
         self.names = names
 
-    def get_geo_coordinates_by_name(self):
+    def get_geo_coordinates(self):
         name_and_coordinates = {}
-        url = "https://maps.googleapis.com/maps/api/geocode/json"
-        for name in self.names:
-            params = {
-                "address": name,
-                "key": settings.GEOCODING_API_KEY
+        no_coord = []
+        url =  "https://places.googleapis.com/v1/places:searchText"
+        headers = {
+                "Content-Type": "application/json",
+                "X-Goog-Api-Key": settings.GEOCODING_API_KEY,
+                "X-Goog-FieldMask": "places.location"
             }
-            response = requests.get(url, params=params)
+        for university_name in self.names:
+            searchText = f"{university_name}".strip().replace("\xa0", " ").replace('\u200b', '')
+            request_json = {"textQuery": searchText}
+            response = requests.post(url, headers=headers, json=request_json)
             if response.status_code != 200:
-                logger.error(f"HTTP error for {name}: {response.status_code}")
-                name_and_coordinates[name] = None
+                logger.error(f"HTTP error for {university_name}: {response.status_code}")
+                name_and_coordinates[university_name] = None
                 continue
-            data = response.json()
-
-            status = data.get("status")
-            if status != "OK":
-                logger.warning(f"Geocoding failed for {name}: {status}. Response: {data}")
-                name_and_coordinates[name] = None
-                continue
-
+            data_response = response.json()
             try:
-                result = data["results"][0]
-                location = result["geometry"]["location"]
-                latitude = location["lat"]
-                longitude = location["lng"]
-                name_and_coordinates[name] = {'lat': latitude, 'lng': longitude}
+                logger.info(f"Trying get coordinates for {university_name}")
+                if "places" in data_response and len(data_response["places"]) > 0:
+                    location = data_response["places"][0]["location"]
+                    name_and_coordinates[university_name] = {
+                        'lat': location["latitude"],
+                        'lng': location["longitude"]
+                    }
+                else:
+                    logger.warning(f"No results for: {university_name}. Response: {data_response}")
+                    name_and_coordinates[university_name] = None
+                    no_coord.append(university_name)
             except (KeyError, IndexError):
-                logger.error(f"Invalid structure for: {name}. Response: {data}")
-                name_and_coordinates[name] = None
-
-            time.sleep(0.2)
-
+                logger.error(f"Invalid structure for: {university_name}. Response: {data_response}")
+                name_and_coordinates[university_name] = None
+            time.sleep(0.5)
+        logger.info(f"Coudn't get coordinates for {no_coord}. Total item = {len(no_coord)}")
         return name_and_coordinates
 
     def save_to_excel_file(self, data, filename="universities_coordinates.xlsx"):
@@ -77,3 +81,10 @@ class GEOCoordinateParserByName:
         save_path = os.path.join(output_dir, filename)
         df.to_excel(save_path, index=False)
         logger.info(f"Excel-файл сохранён по пути: {save_path}")
+
+excel_file =UniversityExcelFile()
+names = excel_file.universities_names
+loc_parser = GEOCoordinateParserByName(names[:10])
+coord = loc_parser.get_geo_coordinates()
+print(coord)
+#loc_parser.save_to_excel_file(coord)
